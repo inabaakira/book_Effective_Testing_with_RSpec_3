9fa26dcb76e9794d4c26a7f5e011b343ebccedce
INABA Akira <inaba.akira@gmail.com>
2019-07-16 07:21:36 +0000

    [Chap.02] Running Just What You Need / RERUNNING EVERYTHING THAT FAILED 終了

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: cpffee_spec.rb
 #    Created:       <2019/07/16 14:32:31>
-#    Last Modified: <2019/07/16 14:49:11>
+#    Last Modified: <2019/07/16 16:29:20>
 
 # frozen_string_literal: true
 
@@ -16,8 +16,12 @@ class Coffee
   end
 
   def price
-    1.00
+    1.00 + ingredients.size * 0.25
   end
+end
+
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
 RSpec.describe 'A cup of coffee' do
