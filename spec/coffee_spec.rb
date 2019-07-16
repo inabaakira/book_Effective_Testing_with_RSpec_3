e062bf5a7f36723fb1811f47bfae908b59c116af
INABA Akira <inaba.akira@gmail.com>
2019-07-16 07:28:56 +0000

    [Chap.02] Running Just What You Need / FOCUSING SPECIFIC EXAMPLES 終了

--- spec/coffee_spec.rb
+++ spec/coffee_spec.rb
@@ -2,7 +2,7 @@
 #-*- mode: ruby; coding: utf-8 -*-
 # file: cpffee_spec.rb
 #    Created:       <2019/07/16 14:32:31>
-#    Last Modified: <2019/07/16 16:29:20>
+#    Last Modified: <2019/07/16 16:40:41>
 
 # frozen_string_literal: true
 
@@ -21,6 +21,7 @@ class Coffee
 end
 
 RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
   config.example_status_persistence_file_path = 'spec/examples.txt'
 end
 
@@ -31,7 +32,7 @@ RSpec.describe 'A cup of coffee' do
     expect(coffee.price).to eq(1.00)
   end
 
-  context 'with milk' do
+  fcontext 'with milk' do
     before { coffee.add :milk }
 
     it 'costs $1.25' do
