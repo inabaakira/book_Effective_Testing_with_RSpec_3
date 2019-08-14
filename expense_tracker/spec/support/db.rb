815617a026493d56f31cf5e8803b31b02a5326c1
INABA Akira <inaba.akira@gmail.com>
2019-08-14 09:39:23 +0000

    [Chap.06] Testing Ledger Behavior

---
+++ expense_tracker/spec/support/db.rb
@@ -0,0 +1,10 @@
+# frozen-string-literal: true
+
+RSpec.configure do |c|
+  c.before(:suite) do
+    Sequel.extension :migration
+    Sequel::Migrator.run(DB, 'db/migrations')
+    DB[:expenses].truncate
+  end
+end
+
